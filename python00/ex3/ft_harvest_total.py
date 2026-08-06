1b91102cf1f737371a40b609afbe16c13ceb9a38
maguzman <marioguzzzman@gmail.com>
2026-08-06 02:19:14 +0200

    add ex0*3

---
+++ python00/ex3/ft_harvest_total.py
@@ -0,0 +1,6 @@
+def ft_harvest_total():
+    d1 = int(input("Day 1 harvest: "))
+    d2 = int(input("Day 2 harvest: "))
+    d3 = int(input("Day 3 harvest: "))
+    total_harvest = d1 + d2 + d3
+    print("Plot area: ", total_harvest)
