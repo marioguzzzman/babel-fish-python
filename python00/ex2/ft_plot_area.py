197dd70cdeb4c8efce8c06951511cb50781954c1
maguzman <marioguzzzman@gmail.com>
2026-08-06 22:51:27 +0200

    adding type hints

--- python00/ex2/ft_plot_area.py
+++ python00/ex2/ft_plot_area.py
@@ -1,5 +1,5 @@
-def ft_plot_area():
+def ft_plot_area() -> None:
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
     area = length * width
-    print("Plot area: ", area)
+    print(f"Plot area: {area}")
--- python00/ex3/ft_harvest_total.py
+++ python00/ex3/ft_harvest_total.py
@@ -1,6 +1,6 @@
-def ft_harvest_total():
+def ft_harvest_total() -> None:
     d1 = int(input("Day 1 harvest: "))
     d2 = int(input("Day 2 harvest: "))
     d3 = int(input("Day 3 harvest: "))
     total_harvest = d1 + d2 + d3
-    print("Plot area: ", total_harvest)
+    print(f"Total harvest: {total_harvest}")
