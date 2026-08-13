1cf719d19e28c7a1bb1a45fcf4f0776f4b2b29eb
maguzman <marioguzzzman@gmail.com>
2026-08-13 16:34:25 +0200

    finish ex4

--- python01/ex4/ft_garden_security.py
+++ python01/ex4/ft_garden_security.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
 
 
 class Plant:
@@ -8,34 +8,61 @@ class Plant:
                  age_days: int,
                  growth_rate: float) -> None:
         self.name = name
-        self.height = height
-        self.age_days = age_days
+        self._height = height
+        self._age_days = age_days
         self.growth_rate = growth_rate
 
     def show(self) -> None:
-        print(f"{self.name}: {self.height:.1f}cm, {self.age_days} days old")
+        print(f"{self.name}: {self._height:.1f}cm, {self._age_days} days old")
 
     def age(self) -> None:
-        self.age_days += 1
+        self._age_days += 1
 
     def grow(self) -> None:
-        self.height += self.growth_rate
+        self._height += self.growth_rate
 
-    def get_height():
+    def validate_data(self, data: float) -> bool:
+        if data < 0:
+            return False
+        return True
 
-    def get_age():
+    def set_height(self, new_height: float) -> bool:
+        if self.validate_data(new_height):
+            self._height = new_height
+            print(f"Height updated: {self._height}cm")
+            return True
+        else:
+            print(f"{self.name}: Error, height can't be negative")
+            return False
+
+    def set_age(self, new_age: int) -> bool:
+        if self.validate_data(new_age):
+            self._age_days = new_age
+            print(f"Age updated: {self._age_days} days")
+            return True
+        else:
+            print(f"{self.name}: Error, age can't be negative")
+            return False
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age_days
+
 
 if __name__ == "__main__":
 
     p1 = Plant("Rose", 25.0, 30, 0.8)
-    p2 = Plant("Sunflower", 80.0, 54, 0.9)
-    p3 = Plant("Cactus", 15.0, 120, 0.1)
-    p4 = Plant("Flower", 5.0, 1, 0.3)
-    p5 = Plant("Palm", 10.0, 12, 0.6)
 
-    garden = [p1, p2, p3, p4, p5]
-
-    print("=== Plant Factory Output ===")
-    for plant in garden:
-        print("Created: ", end="")
-        plant.show()
+    print("=== Garden Security System ===")
+    print("Plant created: ", end="")
+    p1.show()
+    p1.set_height(26.0)
+    p1.set_age(31)
+    if not p1.set_height(-0.6):
+        print("Height update rejected")
+    if not p1.set_age(-30):
+        print("Age update rejected")
+    print("Current state: ", end="")
+    p1.show()
