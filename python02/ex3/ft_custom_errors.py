0956c552176c2c7b9f7fca4c2a18ca020d13a1bf
maguzman <marioguzzzman@gmail.com>
2026-09-24 20:02:16 +0200

    fix error message

--- python02/ex3/ft_custom_errors.py
+++ python02/ex3/ft_custom_errors.py
@@ -2,18 +2,16 @@
 
 class GardenError(Exception):
     def __init__(self, message: str = "Unknown garden error") -> None:
-        self.message = message
+        super().__init__(message)
 
 
 class PlantError(GardenError):
     def __init__(self, message: str = "Unknown plant error") -> None:
-        self.message = message
         super().__init__(message)
 
 
 class WaterError(GardenError):
     def __init__(self, message: str = "Unknown water error") -> None:
-        self.message = message
         super().__init__(message)
 
 
